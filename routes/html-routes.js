2d2666e59fa5c099f08888f5cfc3fe474b2ddaf4
Mark Prodehl <markprodehl@gmail.com>
2020-04-27 15:24:29 -0700

    updated html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -45,6 +45,7 @@ module.exports = function(app) {
       "w3-theme-d1"
     ];
 
+    //This code needs to go into a me
     db.User.findOne({
       attributes: ["email", "id", "challenge"],
       where: {
